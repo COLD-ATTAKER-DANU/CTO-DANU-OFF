e3346ca502c97dc32c62d593e0b2eb0c01dfb0f5
COLD-ATTAKER-DANU <danushakalshan38@gmail.com>
2026-06-12 20:12:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 //gg
 module.exports = {
-SESSION_ID:'put your session id here',
+SESSION_ID:'FmBFGZBK#GSZZLjsLFHwdkYZshz6E22rMAZMP3OYvMB5131-ziXA',
 
 ANTI_DELETE: process.env.ANTI_DELETE === undefined ? 'true' : process.env.ANTI_DELETE, 
 MV_BLOCK: process.env. MV_BLOCK === undefined ? 'false' : process.env. MV_BLOCK,    
